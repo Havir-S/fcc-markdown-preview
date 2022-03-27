ea6a8c565e7646c4725504f85634b2e35308a688
Havir <saffronkrzysztof@gmail.com>
2022-03-27 18:20:04 +0200

    textarea working, need a script to find and translate markdown text into a real html style elements Signed-off-by: Havir <saffronkrzysztof@gmail.com>

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,33 @@
-import { createStore } from '@reduxjs/toolkit';
+import { createStore, combineReducers } from '@reduxjs/toolkit';
 
 
-export const store = createStore({
-  reducer: {
 
-  },
-});
+const defaultState = {
+  text: 'type something here'
+}
+const TEXTUPDATE = 'TEXTUPDATE';
+
+const updateText = () => {
+  return {
+    type: TEXTUPDATE,
+    // text:
+  }
+}
+
+
+const textReducer = ((state = defaultState, action) => {
+  switch (action.type) {
+    case TEXTUPDATE:
+    console.log('we got an update');
+    return state;
+    break;
+    default:
+    return state;
+
+  }
+})
+
+
+
+export const store = createStore(textReducer);
+console.log(store.getState());
--- src/index.js
+++ src/index.js
@@ -1,23 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from '@reduxjs/toolkit';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+const defaultState = {
+  text: ''
 }
+const TEXTUPDATE = 'TEXTUPDATE';
+
+const updateText = (inputValue) => {
+  return {
+    type: TEXTUPDATE,
+    text: inputValue
+  }
+}
+
+const textReducer = ((state = defaultState, action) => {
+  switch (action.type) {
+    case TEXTUPDATE:
+    console.log(action);
+    return state = {text:action.text}
+    break;
+    default:
+    return state;
+  }
+})
+
+const store = createStore(textReducer);
+
+
+function mapStateToProps(state) {
+  return {
+    text: state
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    updateText: (e) => {
+      dispatch(updateText(e));
+    }
+  }
+}
+
+
+class Wrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 's'
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+    this.props.updateText(e.target.value);
+
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className='wrapper'>
+          <div className='textholder'>
+            <textarea value={this.state.text} onChange={this.handleChange} id='editor'>
+            </textarea>
+          </div>
+          <div className='previewholder'>
+            <div id='preview'>
+            {this.state.text}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+const ConnectedWrapper = connect(mapStateToProps,mapDispatchToProps)(Wrapper);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConnectedWrapper />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
